aa44a27583d681695de5c1eaee174606a66a50bb
Pietari Mönkkönen <160852818+Pietari-Monkkonen@users.noreply.github.com>
2025-07-03 15:00:36 +0300

    expand point cloud sampling functionality
    Now the point cloud sampling can be done also in the case where all three LADD marginal distributions are defined. In this case, when point cloud sampling is used, the heightwise LADD is sampled for the leaf height value and then this is used to either use point cloud sampling on that height or alternatively the other two marginal LADDs are used to define leaf start point (depending on the PCSampling weights).
    
    The commit also has minor bug fix.

--- src/canopy-hull-method/sample_leaf_position.m
+++ src/canopy-hull-method/sample_leaf_position.m
@@ -267,7 +267,7 @@ elseif isempty(fDist_d)
                               *[0 1 0]')'*dLeaf ...
                              +[stemCen(1:2) hLeaf];
             % Increment sampling counter and calculate new ratio
-            iPCS = find(hLeaf<PCSampling.binEdges,1,'first') - 1;
+            iPCS = find(hProposal<PCSampling.binEdges,1,'first') - 1;
             PCSampling.nTotalSampled(iPCS) = ...
                                        PCSampling.nTotalSampled(iPCS) + 1;
             PCSampling.ratio(iPCS) = PCSampling.nPCSampled(iPCS)...
@@ -278,52 +278,112 @@ elseif isempty(fDist_d)
 
 
 
-% Use only LADD in positioning
+% Use all three marginal LADD (or point cloud) in positioning
 elseif isa(fDist_h,"function_handle") && isa(fDist_d,"function_handle") ...
        && isa(fDist_c,"function_handle")
 
     accepted = 0;
     while accepted == 0
-        % Proposal values for the variables
-        hProposal = rand(1);
-        dProposal = rand(1);
-        cProposal = 2*pi*rand(1);
-        % LADD value on propsal point
-        funValues = [fDist_h(hProposal),fDist_d(dProposal), ...
-                     fDist_c(cProposal)];
-        vertValues = rand(1,3).*[maxfDist_h,maxfDist_d,maxfDist_c];
-        if all(vertValues < funValues)
-            % Probing the edge of alpha shape
-            edgeValue = stem_to_alphashape_edge(aShape, ...
-                                                hProposal, ...
-                                                cProposal, ...
-                                                maxHorzDist, ...
-                                                zMin, ...
-                                                zMax, ...
-                                                stemCoordinates);
-            % Check the if the distance to edge is nonzero
-            if edgeValue == 0
-                % The first two probe points are not inside the shape,
-                % sample a new proposal point
-                continue
+        % Sample relative height from LADD
+        relHeight = rand(1);
+        if fDist_h(relHeight) > rand(1)*maxfDist_h
+            hLeaf = relHeight*(zMax-zMin) + zMin;
+        else 
+            % Jump to the start of the while loop
+            continue
+        end
+        % Find index of point cloud sampling height bin
+        iPCS = find(relHeight<PCSampling.binEdges,1,'first') - 1;
+        if PCSampling.ratio(iPCS) <= PCSampling.weights(iPCS) ...
+               && PCSampling.flag == true
+            % Corresponding height index in voxelization
+            iz = find(PCSampling.zEdges>hLeaf,1,'first') - 1;
+            % Find all voxels for the height level
+            zSlice = PCSampling.voxels(:,:,iz);
+            zSliceInds = PCSampling.voxelIndex(:,:,iz);
+            zSliceCol = zSlice(:);
+            zSliceIndsCol = zSliceInds(:);
+            % If no voxels on height level, do LADD sampling
+            if sum(zSliceCol) == 0
+                sampleByLADD = true;
+            else
+                cumulativeSum = cumsum(zSliceCol);
+                cumulativePDF = cumulativeSum./cumulativeSum(end);
+                k = find(cumulativePDF>rand(1),1,'first');
+                xyInds = zSliceIndsCol{k};
+                ix = xyInds(1);
+                iy = xyInds(2);
+                % Uniformly sample a point inside the voxel
+                proposal = rand(1,3) ...
+                    .*[PCSampling.xEdges(ix+1)-PCSampling.xEdges(ix) ...
+                    PCSampling.yEdges(iy+1)-PCSampling.yEdges(iy) ...
+                    0] ...
+                    + [PCSampling.xEdges(ix) PCSampling.yEdges(iy) ...
+                    hLeaf];
+                % Set the sampled point as leaf start point
+                leafSP = proposal;
+                % Increment sampling counters and calculate new ratio
+                PCSampling.nPCSampled(iPCS) = ...
+                    PCSampling.nPCSampled(iPCS) + 1;
+                PCSampling.nTotalSampled(iPCS) = ...
+                    PCSampling.nTotalSampled(iPCS) + 1;
+                PCSampling.ratio(iPCS) = PCSampling.nPCSampled(iPCS) ...
+                    /PCSampling.nTotalSampled(iPCS);
+                accepted = 1;
+                sampleByLADD = false;
             end
-            % Position coordinates of the leaf start point
-            hLeaf = hProposal*(zMax-zMin) + zMin;
-            dLeaf = edgeValue*dProposal;
-            cLeaf = cProposal;
-            % Stem center on the corresponding height
-            iSC = find(stemCoordinates(:,3) > hLeaf,1);
-            relPos = (hLeaf-stemCoordinates(iSC-1,3)) ...
-                     /(stemCoordinates(iSC,3)-stemCoordinates(iSC-1,3));
-            stemCen = relPos*(stemCoordinates(iSC,:) ...
-                              - stemCoordinates(iSC-1,:)) ...
-                      + stemCoordinates(iSC-1,:);
-            % Calculate the leaf start point
-            leafSP = (rotation_matrix([0 0 1],cLeaf) ...
-                              *[0 1 0]')'*dLeaf ...
-                             +[stemCen(1:2) hLeaf];
-            accepted = 1;
+        else
+            sampleByLADD = true;
+        end
+
+        if sampleByLADD == true
+            hProposal = relHeight;
+            % Proposal values to relative distance and compass direction
+            dProposal = rand(1);
+            cProposal = 2*pi*rand(1);
+            % Marginal LADD values on proposal points
+            funValues = [fDist_d(dProposal),fDist_c(cProposal)];
+            vertValues = rand(1,2).*[maxfDist_d,maxfDist_c];
+            if all(vertValues < funValues)
+                % Probing the edge of alpha shape
+                edgeValue = stem_to_alphashape_edge(aShape, ...
+                    hProposal, ...
+                    cProposal, ...
+                    maxHorzDist, ...
+                    zMin, ...
+                    zMax, ...
+                    stemCoordinates);
+                % Check the if the distance to edge is nonzero
+                if edgeValue == 0
+                    % The first two probe points are not inside the shape,
+                    % sample a new proposal point
+                    continue
+                end
+                % Position coordinates of the leaf start point
+                hLeaf = hProposal*(zMax-zMin) + zMin;
+                dLeaf = edgeValue*dProposal;
+                cLeaf = cProposal;
+                % Stem center on the corresponding height
+                iSC = find(stemCoordinates(:,3) > hLeaf,1);
+                relPos = (hLeaf-stemCoordinates(iSC-1,3)) ...
+                    /(stemCoordinates(iSC,3)-stemCoordinates(iSC-1,3));
+                stemCen = relPos*(stemCoordinates(iSC,:) ...
+                    - stemCoordinates(iSC-1,:)) ...
+                    + stemCoordinates(iSC-1,:);
+                % Calculate the leaf start point
+                leafSP = (rotation_matrix([0 0 1],cLeaf) ...
+                    *[0 1 0]')'*dLeaf ...
+                    +[stemCen(1:2) hLeaf];
+                % Increment sampling counter and calculate new ratio
+                iPCS = find(hProposal<PCSampling.binEdges,1,'first') - 1;
+                PCSampling.nTotalSampled(iPCS) = ...
+                                       PCSampling.nTotalSampled(iPCS) + 1;
+                PCSampling.ratio(iPCS) = PCSampling.nPCSampled(iPCS)...
+                                         /PCSampling.nTotalSampled(iPCS);
+                accepted = 1;
+            end
         end
     end
-    
+end
+
 end
